7ddb1536e633b404fcf2d5eed6a9e1ca11458997
Tuyền Phan <tuyenphan17122004@gmail.com>
2024-11-08 17:39:36 +0700

    deloy backend

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v2',
+    baseURL: 'https://buysourcecode-production.up.railway.app/api/v2',
     timeout: 5000,
 });
 
