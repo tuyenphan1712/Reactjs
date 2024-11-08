1453f6710b667daa87bbb97ceb8eaea4b2bf5a11
Tuyền Phan <tuyenphan17122004@gmail.com>
2024-11-08 18:35:19 +0700

    domain

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://buysourcecode-production.up.railway.app/api/v2',
+    baseURL: 'https://api.sourceuit.tech/api/v2',
     timeout: 5000,
 });
 
